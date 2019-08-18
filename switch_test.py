b3f021f57bba4219405a237a34adc5c5aa3e1c0a
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-08-18 19:34:46 -0400

    update

--- switch_test.py
+++ switch_test.py
@@ -3,6 +3,8 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN)
-GPIO.setup(6, GPIO.IN)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-print(GPIO.input(5))
+while True:
+    print(GPIO.input(5))
+    print(GPIO.input(6))
