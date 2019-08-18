ed5ee778d3b45f16728cc6410254290fa9e0cf9b
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-08-18 19:55:52 -0400

    update

--- DRV8825.py
+++ DRV8825.py
@@ -79,22 +79,22 @@ class DRV8825():
 
     def TurnStep_test(self, Dir, steps, stepdelay=0.005):
         if (Dir == MotorDir[0]):
-            print("forward")
+            # print("forward")
             self.digital_write(self.enable_pin, 0)
             self.digital_write(self.dir_pin, 0)
         elif (Dir == MotorDir[1]):
-            print("backward")
+            # print("backward")
             self.digital_write(self.enable_pin, 0)
             self.digital_write(self.dir_pin, 1)
         else:
-            print("the dir must be : 'forward' or 'backward'")
+            # print("the dir must be : 'forward' or 'backward'")
             self.digital_write(self.enable_pin, 1)
             return
 
         if (steps == 0):
             return
 
-        print("turn step: ",steps)
+        # print("turn step: ",steps)
         for i in range(steps):
             self.digital_write(self.step_pin, True)
             time.sleep(stepdelay)
