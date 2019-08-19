4bc598bdcbae452a934ac49c51e59e58dddf5c5b
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-08-18 21:17:59 -0400

    update

--- calibration_test.py
+++ calibration_test.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from DRV8825 import DRV8825
 from time import sleep
 
-M_Rot = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+# M_Rot = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 M_Lin = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
 outer_switch = 5
