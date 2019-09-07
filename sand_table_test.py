aeb28732c182e551e3082ae648e62e00e8d67d78
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-07 16:15:38 -0400

    update

--- sand_table_test.py
+++ sand_table_test.py
@@ -44,6 +44,44 @@ def get_coordinates(filename, mypath):
 
     return coor
 
+def calibrate_slide():
+    outer_switch = 5
+    inner_switch = 6
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(outer_switch, GPIO.IN)
+    GPIO.setup(inner_switch, GPIO.IN)
+
+    delay = 1 / 1000
+    pos = 0
+    center_to_min = 20
+    outer_to_max = 20
+
+    calibrated = False
+
+    while not calibrated:
+        minPos = M_Lin.Turn(Dir='backward', limit_switch=inner_switch, stepdelay=delay)
+        maxPos = M_Lin.Turn(Dir='forward', limit_switch=outer_switch, stepdelay=delay) + minPos
+
+        positions = (minPos, maxPos)
+        print(positions)
+        totalDist = maxPos - minPos - center_to_min - outer_to_max
+        print ("Travel Distance: " + str(totalDist))
+
+        sleep(2)
+
+        test_inner = M_Lin.TurnStep_test(Dir='backward', steps=totalDist + outer_to_max, limit_switch=inner_switch, stepdelay=delay)
+        minPos = 0
+        sleep(2)
+        test_outer = M_Lin.TurnStep_test(Dir='forward', steps=totalDist, limit_switch=outer_switch, stepdelay=delay)
+        maxPos = totalDist
+        if test_inner and test_outer:
+            calibrated = True
+        else:
+            print("Calibration Failed! Trying again...")
+    print("Calibration Passed!")
+    return totalDist
+
 def stop_program(threading_event):
     threading_event.set()
     MRot.join()
@@ -61,6 +99,7 @@ default_speed = 1000
 max_speed = 2000
 
 try:
+    #maxDisp = calibrate_slide()
     threading_event = threading.Event()
     files = get_files(mypath)
     for f in files:
