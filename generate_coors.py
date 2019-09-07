f2359b1ca87352438f83339ad5c5fe22e12626ee
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-07 17:31:23 -0400

    update

---
+++ generate_coors.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import isfile, join
+
+def get_files(mypath):
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    print("Files found: " + str(onlyfiles).replace('[', '').replace(']', ''))
+
+    return onlyfiles
+
+def generate_coordinates(mypath, fileName, thetaStep, funcRange):
+    theta = funcRange[0]
+
+    f=open("file{}.txt".format(str(num)))
