3ef4c8722e54257bcaf6fd04d3a4a80ccbece722
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-07 19:13:20 -0400

    update

--- led_strip_test.py
+++ led_strip_test.py
@@ -5,7 +5,7 @@ import neopixel
 
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+# NeoPixels must be connected to D10, sudo o work.
 pixel_pin = board.D10
 
 # The number of NeoPixels
@@ -13,7 +13,7 @@ num_pixels = 20
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.RGB
+ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
                            pixel_order=ORDER)
