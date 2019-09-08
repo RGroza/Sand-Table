d540c4c10d40d3dd60c0b81484b6558276a76963
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-08 00:17:26 -0400

    made generate_coors

--- generate_coors.py
+++ generate_coors.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+import math
 
 def get_files(mypath):
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -7,7 +8,18 @@ def get_files(mypath):
 
     return onlyfiles
 
-def generate_coordinates(mypath, fileName, thetaStep, funcRange):
-    theta = funcRange[0]
+def generate_coordinates(mypath, coor_amt, revs):
+    f = open(mypath + "testfile.txt", "w")
 
-    #f=open("file{}.txt".format(str(num)))
+    rev_steps = 3200
+    arm_length = 1900
+    currentTheta = 0
+    theta_steps = round(revs * rev_steps / coor_amt)
+
+    for i in range(coor_amt):
+        funcResult = round(arm_length * abs(math.cos(math.radians(360 * currentTheta / rev_steps))))
+        f.write(str(currentTheta) + " " + str(funcResult) + "\n")
+        currentTheta += theta_steps
+    print("Done!")
+
+generate_coordinates("files/", 50, 3)
