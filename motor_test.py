68198ebc4afbef3856dd2a8c1bbedbadef2c5a8a
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-11 23:19:17 -0400

    update

--- motor_test.py
+++ motor_test.py
@@ -7,20 +7,20 @@ import keyboard
 M_Rot = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 M_Lin = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
-def run_MRot(num_steps, delay, stop_event):
+def run_MRot(num_steps, delay):
     M_Rot.SetMicroStep('software','fullstep')
     if num_steps > 0:
-        M_Rot.TurnStep(stop_event, Dir='forward', steps=num_steps, stepdelay = delay)
+        M_Rot.TurnStep_test(Dir='forward', steps=num_steps, stepdelay = delay)
     else:
-        M_Rot.TurnStep(stop_event, Dir='backward', steps=abs(num_steps), stepdelay = delay)
+        M_Rot.TurnStep_test(Dir='backward', steps=abs(num_steps), stepdelay = delay)
     M_Rot.Stop()
 
-def run_MLin(num_steps, delay, stop_event):
+def run_MLin(num_steps, delay):
     M_Lin.SetMicroStep('software','fullstep')
     if num_steps > 0:
-        M_Lin.TurnStep(stop_event, Dir='forward', steps=num_steps, stepdelay = delay)
+        M_Lin.TurnStep_test(Dir='forward', steps=num_steps, stepdelay = delay)
     else:
-        M_Lin.TurnStep(stop_event, Dir='backward', steps=abs(num_steps), stepdelay = delay)
+        M_Lin.TurnStep_test(Dir='backward', steps=abs(num_steps), stepdelay = delay)
     M_Lin.Stop()
 
 coors = [0, 500, 1000]
