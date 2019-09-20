25687cda2107e93f5267341af55ddd10275d0cae
Roberto Groza <robertogroza@gmail.com>
2019-09-20 13:43:42 -0400

    Update table_led.py

--- table_led.py
+++ table_led.py
@@ -6,8 +6,8 @@ from time import sleep
 import keyboard
 from rpi_ws281x import PixelStrip, Color
 
-    
-isStillMoving = False #flag that motors are to be moving
+
+isStillMoving = False # Flag whether motors are to be moving
 
 # LED strip configuration:
 LED_COUNT = 52        # Number of LED pixels.
@@ -17,7 +17,7 @@ LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL = 0       # Set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 M_Rot = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
@@ -26,89 +26,7 @@ M_Lin = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 # Create NeoPixel object with appropriate configuration.
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
-
-def run_LedStrip(stop_event):
-    strip.begin()
-
-    while isStillMoving:
-        print('Color wipe animations.')
-        colorWipe(strip, Color(255, 0, 0))  # Red wipe
-        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-        colorWipe(strip, Color(0, 0, 255))  # Green wipe
-        print('Theater chase animations.')
-        theaterChase(strip, Color(127, 127, 127))  # White theater chase
-        theaterChase(strip, Color(127, 0, 0))  # Red theater chase
-        theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
-        print('Rainbow animations.')
-        rainbow(strip)
-        rainbowCycle(strip)
-        theaterChaseRainbow(strip)
-    
-
-def run_MRotate(stop_event):
-    M_Rot.SetMicroStep('software','1/4step')
-    rot_delay = 0.0015
-    rot_steps = 3200
-    while isStillMoving:
-        M_Rot.TurnStep_ROT(Dir='forward', steps=rot_steps, stepdelay = rot_delay)
-    M_Rot.Stop()
-
-def run_MLinear(num_steps, delay, stop_event):
-    M_Lin.SetMicroStep('software','1/4step')
-    if num_steps > 0:
-        M_Lin.TurnStep(stop_event, Dir='forward', steps=num_steps, stepdelay = delay)
-    else:
-        M_Lin.TurnStep(stop_event, Dir='backward', steps=abs(num_steps), stepdelay = delay)
-
-def run_MLin(num_steps, delay, stop_event):
-    M_Lin.SetMicroStep('software','1/4step')
-    if num_steps > 0:
-        M_Lin.TurnStep(stop_event, Dir='forward', steps=num_steps, stepdelay = delay)
-    else:
-        M_Lin.TurnStep(stop_event, Dir='backward', steps=abs(num_steps), stepdelay = delay)
-    M_Lin.Stop()
-
-def calibrate_slide():
-    outer_switch = 5
-    inner_switch = 6
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(outer_switch, GPIO.IN)
-    GPIO.setup(inner_switch, GPIO.IN)
-
-    delay = 1 / 1000
-    center_to_min = 20
-    outer_to_max = 20
-
-    calibrated = False
-
-    while not calibrated:
-        minPos = M_Lin.Turn(Dir='backward', limit_switch=inner_switch, stepdelay=delay)
-        maxPos = M_Lin.Turn(Dir='forward', limit_switch=outer_switch, stepdelay=delay) + minPos
-
-        positions = (minPos, maxPos)
-        print(positions)
-        totalDist = maxPos - minPos - center_to_min - outer_to_max
-        print ("Travel Distance: " + str(totalDist))
-
-        sleep(2)
-
-        test_inner = M_Lin.TurnStep_cali(Dir='backward', steps=totalDist + outer_to_max, limit_switch=inner_switch, stepdelay=delay)
-        minPos = 0
-        sleep(2)
-        test_outer = M_Lin.TurnStep_cali(Dir='forward', steps=totalDist, limit_switch=outer_switch, stepdelay=delay)
-        maxPos = totalDist
-        test_inner = M_Lin.TurnStep_cali(Dir='backward', steps=totalDist, limit_switch=inner_switch, stepdelay=delay)
-
-        if test_inner and test_outer:
-            calibrated = True
-        else:
-            print("Calibration Failed! Trying again...")
-    print("Calibration Passed!")
-
-    return totalDist
-
-# Define functions which animate LEDs in various ways.
+# LED strip funcitons
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
@@ -172,6 +90,85 @@ def theaterChaseRainbow(strip, wait_ms=50):
                 strip.setPixelColor(i + q, 0)
 
 
+# Run through the LED strip routine
+def run_LedStrip(stop_event):
+    strip.begin()
+
+    while isStillMoving:
+        print('Color wipe animations.')
+        colorWipe(strip, Color(255, 0, 0))  # Red wipe
+        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+        colorWipe(strip, Color(0, 0, 255))  # Green wipe
+        print('Theater chase animations.')
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127, 0, 0))  # Red theater chase
+        theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
+        print('Rainbow animations.')
+        rainbow(strip)
+        rainbowCycle(strip)
+        theaterChaseRainbow(strip)
+
+
+# Functions defined for each motor thread
+def run_MRotate(stop_event):
+    M_Rot.SetMicroStep('software','1/4step')
+    rot_delay = 0.0015
+    rot_steps = 3200
+    while isStillMoving:
+        M_Rot.TurnStep_ROT(Dir='forward', steps=rot_steps, stepdelay = rot_delay)
+    M_Rot.Stop()
+
+def run_MLinear(num_steps, delay, stop_event):
+    M_Lin.SetMicroStep('software','1/4step')
+    if num_steps > 0:
+        M_Lin.TurnStep(stop_event, Dir='forward', steps=num_steps, stepdelay = delay)
+    else:
+        M_Lin.TurnStep(stop_event, Dir='backward', steps=abs(num_steps), stepdelay = delay)
+
+
+# Calibrates the linear slide arm before starting the main program routine
+def calibrate_slide():
+    outer_switch = 5
+    inner_switch = 6
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(outer_switch, GPIO.IN)
+    GPIO.setup(inner_switch, GPIO.IN)
+
+    delay = 1 / 1000
+    center_to_min = 20
+    outer_to_max = 20
+
+    calibrated = False
+
+    while not calibrated:
+        minPos = M_Lin.Turn(Dir='backward', limit_switch=inner_switch, stepdelay=delay)
+        maxPos = M_Lin.Turn(Dir='forward', limit_switch=outer_switch, stepdelay=delay) + minPos
+
+        positions = (minPos, maxPos)
+        print(positions)
+        totalDist = maxPos - minPos - center_to_min - outer_to_max
+        print ("Travel Distance: " + str(totalDist))
+
+        sleep(2)
+
+        test_inner = M_Lin.TurnStep_cali(Dir='backward', steps=totalDist + outer_to_max, limit_switch=inner_switch, stepdelay=delay)
+        minPos = 0
+        sleep(2)
+        test_outer = M_Lin.TurnStep_cali(Dir='forward', steps=totalDist, limit_switch=outer_switch, stepdelay=delay)
+        maxPos = totalDist
+        test_inner = M_Lin.TurnStep_cali(Dir='backward', steps=totalDist, limit_switch=inner_switch, stepdelay=delay)
+
+        if test_inner and test_outer:
+            calibrated = True
+        else:
+            print("Calibration Failed! Trying again...")
+    print("Calibration Passed!")
+
+    return totalDist
+
+
+# Stops the motors and LED strip, and joins the threads
 def stop_program(threading_event):
     threading_event.set()
     isStillMoving = False
@@ -186,37 +183,49 @@ def stop_program(threading_event):
     print("Exiting...")
     exit()
 
-default_speed = 750
-max_speed = 1000
-rev_steps = 3200
-currentTheta = 0 # theta coordinate val - currently just incrementer
-theta_steps = 100
-LinPos = 0
-LastLinPos = 0
-Lin_delay = 0.00125
 
-try:
-    maxDisp = calibrate_slide() - 50
-    threading_event = threading.Event()
-    isStillMoving = True
+# Create a function and return the linear postion for the arm (r coordinate val)
+def draw_function(maxDisp, currentTheta, rev_steps):
+    # Function that can be modified to create different patterns
+    pos = round(maxDisp * abs(math.cos(math.radians(360 * currentTheta / rev_steps))))
+    return pos
 
-    # start Rotation
-    MRot = threading.Thread(target=run_MRotate, args=(threading_event,))
-    LStrip = threading.Thread(target=run_LedStrip, args=(threading_event,))
-    MRot.start()
-    print("\nROT Thread Started")
-    LStrip.start()
-    print("\nLed Strip Thread Started")
 
-    while isStillMoving:
-        LastLinPos = LinPos
-        LinPos = round(maxDisp * abs(math.cos(math.radians(360 * currentTheta / rev_steps)))) # r coordinate val
-        currentTheta += theta_steps
+# Main function for the program
+def main():
+    default_speed = 750
+    max_speed = 1000
+    rev_steps = 3200
+    currentTheta = 0 # Theta coordinate val - currently just an incrementer
+    theta_steps = 100
+    LinPos = 0
+    LastLinPos = 0
+    Lin_delay = 0.00125
 
-        print(str(LinPos))
+    try:
+        maxDisp = calibrate_slide() - 50
+        threading_event = threading.Event()
+        isStillMoving = True
 
-        nextPos = LinPos - LastLinPos
-        run_MLinear (nextPos, Lin_delay, threading_event,)
+        # Start rotation, split into 3 threads (the main thread will process linear movements for MLin)
+        MRot = threading.Thread(target=run_MRotate, args=(threading_event,))
+        LStrip = threading.Thread(target=run_LedStrip, args=(threading_event,))
+        MRot.start()
+        print("\nROT Thread Started")
+        LStrip.start()
+        print("\nLed Strip Thread Started")
 
-except KeyboardInterrupt:
-    stop_program(threading_event)
+        while isStillMoving:
+            LastLinPos = LinPos
+            LinPos = draw_function(maxDisp, currentTheta, rev_steps) # Set the LinPos to the calculated position
+            currentTheta += theta_steps
+
+            print(str(LinPos))
+
+            nextPos = LinPos - LastLinPos
+            run_MLinear(nextPos, Lin_delay, threading_event,)
+
+    except KeyboardInterrupt:
+        stop_program(threading_event)
+
+main()
