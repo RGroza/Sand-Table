5b248493311672630a405fc7eba9d2458394712c
Roberto Groza <robertogroza@gmail.com>
2019-09-23 11:39:18 -0400

    created plotting_coors() which allows text coors to be plotted in desmos

--- generate_coors.py
+++ generate_coors.py
@@ -8,18 +8,45 @@ def get_files(mypath):
 
     return onlyfiles
 
-def generate_coordinates(mypath, coor_amt, revs):
-    f = open(mypath + "testfile.txt", "w")
+# Generates coordinates that are interpreted by the main program
+def stepper_coordinates(mypath, file_name, coor_amt, revs):
+    f = open(mypath + file_name, "w")
 
     rev_steps = 3200
     arm_length = 1900
-    currentTheta = 0 # theta coordinate val
+    currentTheta = 0 # theta coordinate val in steps
     theta_steps = round(rev_steps / coor_amt)
 
     for i in range(revs * coor_amt):
         funcResult = round(arm_length * abs(math.cos(math.radians(360 * currentTheta / rev_steps)))) # r coordinate val
-        f.write(str(currentTheta) + " " + str(funcResult) + "\n")
+        f.write("(" + str(currentTheta) + ", " + str(funcResult) + ")\n")
         currentTheta += theta_steps
     print("Done!")
 
-generate_coordinates("files/", 100, 3)
+# Generates coordinates that can be plotted by desmos
+# mypath: location of file created
+# file_name: name of file
+# coor_amt: num of coor per rev
+# revs: num of revs
+# max_amp: max amplitude of the function
+def plotting_coordinates(mypath, file_name, coor_amt, revs, max_amp):
+    f = open(mypath + file_name, "w")
+
+    rev_steps = 3200
+    max_disp = max_amp
+    currentTheta = 0 # theta coordinate val in steps
+    theta_steps = round(rev_steps / coor_amt)
+
+    for i in range(revs):
+        for i in range(coor_amt):
+            funcResult = round(max_disp * abs(math.cos(math.radians(360 * currentTheta / rev_steps)))) # r coordinate val
+
+            f.write("\left(" + str(funcResult) + "\cos\left(" + str(round(360*(currentTheta / rev_steps), 1)) + "\\right),\ "
+                             + str(funcResult) + "\sin\left(" + str(round(360*(currentTheta / rev_steps), 1)) +  "\\right)\\right)\n")
+
+            currentTheta += theta_steps
+        currentTheta = 0
+    print("Done!")
+
+#stepper_coordinates("files/", "testfile.txt", 100, 1)
+plotting_coordinates("files/", "testfile.txt", 100, 1, 100)
