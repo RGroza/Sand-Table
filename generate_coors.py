c0fdae089d2373c1baa386070602034247099c8d
HomePC\Roberto Groza <robertogroza@gmail.com>
2019-09-24 00:34:15 -0400

    update to generating test coors

--- generate_coors.py
+++ generate_coors.py
@@ -23,6 +23,26 @@ def stepper_coordinates(mypath, file_name, coor_amt, revs):
         currentTheta += theta_steps
     print("Done!")
 
+def simple_square(deg, max_amp):
+    if deg > 360:
+        deg -= 360
+
+    if deg >= 0 and deg < 45 or deg > 315 and deg <= 360:
+        funcResult = round(max_amp / math.cos(math.radians(deg)))
+    elif deg > 45 and deg < 135:
+        funcResult = round(max_amp / math.sin(math.radians(deg)))
+    elif deg > 135 and deg < 225:
+        funcResult = round(-max_amp / math.cos(math.radians(deg)))
+    else:
+        funcResult = round(-max_amp / math.sin(math.radians(deg)))
+    return funcResult
+
+# def square_spiral(deg, max_amp, layers):
+#     amp = max_amp
+#     layer_sep = round(max_amp / layers)
+#     for l in layers:
+#         for n in range(3):
+
 # Generates coordinates that can be plotted by desmos
 # mypath: location of file created
 # file_name: name of file
@@ -32,30 +52,19 @@ def stepper_coordinates(mypath, file_name, coor_amt, revs):
 def plotting_coordinates(mypath, file_name, coor_amt, revs, max_amp):
     f = open(mypath + file_name, "w")
 
-    rev_steps = 3200
-    max_disp = max_amp
+    rev_steps = 3200 # default sand table mechanism setting for steps per rev
     currentTheta = 0 # theta coordinate val in steps
     theta_steps = round(rev_steps / coor_amt)
 
-    for i in range(revs):
-        for i in range(coor_amt):
-            # funcResult = round(max_disp * abs(math.sin(math.radians(360 * currentTheta / rev_steps)))) # r coordinate val
-            deg = 360 * currentTheta / rev_steps
-            if deg >= 0 and deg < 45 or deg > 315 and deg <= 360:
-                funcResult = round(max_disp / math.cos(math.radians(deg))) # r coordinate val
-            elif deg > 45 and deg < 135:
-                funcResult = round(max_disp / math.sin(math.radians(deg))) # r coordinate val
-            elif deg > 135 and deg < 225:
-                funcResult = round(-max_disp / math.cos(math.radians(deg))) # r coordinate val
-            else:# deg > 215 and deg < 315:
-                funcResult = round(-max_disp / math.sin(math.radians(deg))) # r coordinate val
+    for i in range(revs * coor_amt):
+        deg = 360 * currentTheta / rev_steps
+        funcResult = simple_square(deg, max_amp) # r coordinate val
 
-            f.write("\left(" + str(funcResult) + "\cos\left(" + str(round((deg), 1)) + "\\right),\ "
-                             + str(funcResult) + "\sin\left(" + str(round((deg), 1)) +  "\\right)\\right)\n")
+        f.write("\left(" + str(funcResult) + "\cos\left(" + str(round((deg), 1)) + "\\right),\ "
+                            + str(funcResult) + "\sin\left(" + str(round((deg), 1)) +  "\\right)\\right)\n")
 
-            currentTheta += theta_steps
-        currentTheta = 0
+        currentTheta += theta_steps
     print("Done!")
 
 #stepper_coordinates("files/", "testfile.txt", 100, 1)
-plotting_coordinates("files/", "testfile2.txt", 100, 1, 100)
+plotting_coordinates("files/", "testfile2.txt", 200, 2, 100)
