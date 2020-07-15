13f3024fee57ff9dedd3da0b338a29f1a213ac30
RGroza <robertogroza@gmail.com>
2020-07-15 13:00:18 -0400

    changes on pi

--- microseconds_test.py
+++ microseconds_test.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from ..utils.timing import delayMicroseconds
+from utils.timing import delayMicroseconds
 
 # for i in range(10):
 #     begin = datetime.now()
@@ -7,10 +7,11 @@ from ..utils.timing import delayMicroseconds
 #     while ((end.second - begin.second) * 1000000 + end.microsecond - begin.microsecond) < 10:
 #         end = datetime.now()
 
-begin = datetime.now()
-delayMicroseconds(10)
-end = datetime.now()
+for i in range(10):
+    begin = datetime.now()
+    delayMicroseconds(10)
+    end = datetime.now()
 
-print(begin)
-print(end)
-print(end - begin)
+    print(begin)
+    print(end)
+    print(end - begin)
