08a4e478f759c4975ea406e8cd287a3da0d80fbe
RGroza <robertogroza@gmail.com>
2020-07-15 20:02:44 -0400

    Update led_strip.py

--- led_strip.py
+++ led_strip.py
@@ -96,17 +96,17 @@ if __name__ == '__main__':
 
         while True:
             print('Color wipe animations.')
-            colorWipe(strip, Color(255, 0, 0))  # Red wipe
-            colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-            colorWipe(strip, Color(0, 0, 255))  # Green wipe
+            strip.colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            strip.colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+            strip.colorWipe(strip, Color(0, 0, 255))  # Green wipe
             print('Theater chase animations.')
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127, 0, 0))  # Red theater chase
-            theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
+            strip.theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            strip.theaterChase(strip, Color(127, 0, 0))  # Red theater chase
+            strip.theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
             print('Rainbow animations.')
-            rainbow(strip)
-            rainbowCycle(strip)
-            theaterChaseRainbow(strip)
+            strip.rainbow(strip)
+            strip.rainbowCycle(strip)
+            strip.theaterChaseRainbow(strip)
 
     except KeyboardInterrupt:
         if args.clear:
