4bcea977914f7d659ac17bd0ff6186f36e73694d
RGroza <robertogroza@gmail.com>
2020-07-15 20:01:24 -0400

    Update led_strip.py

--- led_strip.py
+++ led_strip.py
@@ -22,61 +22,55 @@ LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(self, strip, color, wait_ms=50):
-"""Wipe color across display a pixel at a time."""
-for i in range(strip.numPixels()):
-    strip.setPixelColor(i, color)
-    strip.show()
-    time.sleep(wait_ms / 1000.0)
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
 
 def theaterChase(self, strip, color, wait_ms=50, iterations=10):
-"""Movie theater light style chaser animation."""
-for j in range(iterations):
-    for q in range(3):
-        for i in range(0, strip.numPixels(), 3):
-            strip.setPixelColor(i + q, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        for i in range(0, strip.numPixels(), 3):
-            strip.setPixelColor(i + q, 0)
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, color)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
 
 def wheel(self, pos):
-"""Generate rainbow colors across 0-255 positions."""
-if pos < 85:
-    return Color(pos * 3, 255 - pos * 3, 0)
-elif pos < 170:
-    pos -= 85
-    return Color(255 - pos * 3, 0, pos * 3)
-else:
-    pos -= 170
-    return Color(0, pos * 3, 255 - pos * 3)
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
 
 def rainbow(self, strip, wait_ms=20, iterations=1):
-"""Draw rainbow that fades across all pixels at once."""
-for j in range(256 * iterations):
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, self.wheel((i + j) & 255))
-    strip.show()
-    time.sleep(wait_ms / 1000.0)
-
-def rainbowCycle(self, strip, wait_ms=20, iterations=5):
-"""Draw rainbow that uniformly distributes itself across all pixels."""
-for j in range(256 * iterations):
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, self.wheel(
-            (int(i * 256 / strip.numPixels()) + j) & 255))
-    strip.show()
-    time.sleep(wait_ms / 1000.0)
-
-def theaterChaseRainbow(self, strip, wait_ms=50):
-"""Rainbow movie theater light style chaser animation."""
-for j in range(256):
-    for q in range(3):
-        for i in range(0, strip.numPixels(), 3):
-            strip.setPixelColor(i + q, self.wheel((i + j) % 255))
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, self.wheel((i + j) & 255))
         strip.show()
         time.sleep(wait_ms / 1000.0)
-        for i in range(0, strip.numPixels(), 3):
-            strip.setPixelColor(i + q, 0)
+
+def rainbowCycle(self, strip, wait_ms=20, iterations=5):
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, self.wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def theaterChaseRainbow(self, strip, wait_ms=50):
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, self.wheel((i + j) % 255))
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
 
 
 def strip_init():
@@ -116,4 +110,4 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         if args.clear:
-            strip_thread.colorWipe(strip, Color(0, 0, 0), 10)
+            colorWipe(strip, Color(0, 0, 0), 10)
