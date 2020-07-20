88366f054d3487570728cc485b3665f98ab065e7
RGroza <robertogroza@gmail.com>
2020-07-20 14:08:06 -0400

    Update relay_test.py

--- tests/relay_test.py
+++ tests/relay_test.py
@@ -3,9 +3,20 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
-GPIO.output(16, GPIO.HIGH)
-sleep(2)
 GPIO.output(16, GPIO.LOW)
+print("Motors ON")
+sleep(2)
+GPIO.output(16, GPIO.HIGH)
+print("Motors OFF")
+
+sleep(1)
+
+GPIO.output(22, GPIO.LOW)
+print("LEDs ON")
+sleep(2)
+GPIO.output(22, GPIO.HIGH)
+print("LEDs OFF")
 
 GPIO.cleanup()
