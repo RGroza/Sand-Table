70e053bd6a073ccc9c4bee9ae9dce9582bc73e0e
RGroza <robertogroza@gmail.com>
2020-07-20 22:01:22 -0400

    Update lcd_test.py

--- tests/lcd_test.py
+++ tests/lcd_test.py
@@ -1,5 +1,12 @@
 from utils import i2c_lcd_driver
+from time import sleep
 
 lcd = i2c_lcd_driver.lcd()
 
 lcd.lcd_display_string("Hello World!", 1)
+lcd.lcd_display_string("Hello World!", 2)
+
+sleep(1)
+
+lcd.lcd_display_string("World Hello!", 1)
+lcd.lcd_display_string("World Hello!", 2)
